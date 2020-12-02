56b17b28b872cce1820b3098495f3e5af7167e28
Yohannes Berhanu <Johnlights51@gmail.com>
2020-12-02 22:35:03 +0300

    Deployed

--- src/Pages/AllHotels/components/Hotels.jsx
+++ src/Pages/AllHotels/components/Hotels.jsx
@@ -1,11 +1,28 @@
 import React from "react";
-import { Card, CardHeader } from "reactstrap";
+import { Card, CardBody, CardHeader, CardImg, Col, Row } from "reactstrap";
+import { Intercontinental } from "../../../Assets/HotelImages/HotelView";
 
 const HotelComponent = () => {
   return (
     <div>
-      <Card>
+      <Card className="border-0">
         <CardHeader>Hotel Component</CardHeader>
+        <CardBody>
+          <Card row className="AllItemsHotelContainer">
+            <Row>
+              <Col md={4} sm={12} xs={12}>
+                <CardImg src={Intercontinental} />
+              </Col>
+
+              <Col className="hotelNames" md={8} sm={12} xs={12}>
+                <CardHeader className="border-0">
+                  Skylight International Hotel
+                </CardHeader>
+                <CardHeader className="border-0">Rating Goes Here</CardHeader>
+              </Col>
+            </Row>
+          </Card>
+        </CardBody>
       </Card>
     </div>
   );

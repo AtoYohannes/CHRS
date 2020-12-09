d1b93746c7e758cd8547dc68410cda07c03d37ec
Yohannes Berhanu <Johnlights51@gmail.com>
2020-12-09 12:56:13 +0300

    Finished With Settings Page

---
+++ src/Pages/Admin/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Waxxaaa</h1>
+    </div>
+  );
+};
+export default Index;
--- src/Pages/SettingsPage/index.jsx
+++ src/Pages/SettingsPage/index.jsx
@@ -1,10 +1,60 @@
 import React from "react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Col,
+  FormGroup,
+  Input,
+  Label,
+  Row,
+} from "reactstrap";
+import Avatar from "../../Components/Avatar";
 
 const Settings = () => {
   return (
-    <div>
-      <h1>Settings</h1>
-    </div>
+    <Card className="settingsContainer">
+      <CardHeader align="center">
+        <Avatar size={250} />
+        <Button className="mt-3" outline>
+          Change Profile Picture
+        </Button>
+      </CardHeader>
+      <CardBody>
+        <Row>
+          <Col md={6} sm={12} xs={12}>
+            <FormGroup>
+              <Label>First Name</Label>
+              <Input placeholder="First Name" />
+            </FormGroup>
+          </Col>
+          <Col md={6} sm={12} xs={12}>
+            <FormGroup>
+              <Label>Last Name</Label>
+              <Input placeholder="Last Name" />
+            </FormGroup>
+          </Col>
+          <Col md={6} sm={12} xs={12}>
+            <FormGroup>
+              <Label>Email</Label>
+              <Input placeholder="Email" type="email" />
+            </FormGroup>
+          </Col>
+          <Col md={6} sm={12} xs={12}>
+            <FormGroup>
+              <Label>Phone Number</Label>
+              <Input placeholder="Phone Number" type="number" />
+            </FormGroup>
+          </Col>
+        </Row>
+      </CardBody>
+
+      <CardFooter align="center">
+        <Button outline>Save Changes</Button>
+      </CardFooter>
+    </Card>
   );
 };
 export default Settings;

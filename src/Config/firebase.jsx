1aaf67fbed35400519928d6f5a3935f4d2a782e8
Yohannes Berhanu <Johnlights51@gmail.com>
2020-12-16 12:53:06 +0300

    Pushed

---
+++ src/Config/firebase.jsx
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAHN1_jM-DkZfxwOfJgpzg9qZTatIWnIPE",
+  authDomain: "chrs-90ba4.firebaseapp.com",
+  databaseURL: "https://chrs-90ba4.firebaseio.com",
+  projectId: "chrs-90ba4",
+  storageBucket: "chrs-90ba4.appspot.com",
+  messagingSenderId: "424745087738",
+  appId: "1:424745087738:web:13e3c7b5733a914ecc4f43",
+  measurementId: "G-PL4Q7XNPYW",
+};
+
+firebase.initializeApp(firebaseConfig);
